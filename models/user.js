b739de7421a4a6de5bd186f9dd4e75cbd7010983
Jérémy DUTHEIL <dutheil.jeremy@gmail.com>
2016-11-28 15:46:39 +0100

    users

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-	username: { type: String, unique: true, required: true },
+	username: { type: String, index: true, unique: true, required: true },
 	password: { type: String, required: true }
 });
 
